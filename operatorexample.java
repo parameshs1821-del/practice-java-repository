d3d12f61d668c7c6b6c11920a700228afb7ee461
parameshs1821-del <parameshs1821@gmail.com>
2026-03-16 23:32:02 +0530

    Updated Java practice files

---
+++ operatorexample.java
@@ -0,0 +1,11 @@
+public class operatorexample {
+    public static void main(String[] args){
+        int a = 20;
+        int b= 10 ;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+    }
+    
+}
