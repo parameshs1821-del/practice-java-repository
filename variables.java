79959384a55f041bc8524a4192bbcb22e5b8db45
parameshs1821-del <parameshs1821@gmail.com>
2026-03-16 23:22:23 +0530

    Updated Java practice files

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables{
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 12.12;
+        char f = 'c';
+        float c = 15.5f;
+        boolean d = false;
+        String name = "darshan";
+        System.out.println("integer value:" +a);
+        System.out.println("double value:"+b);
+        System.out.println("char value:"+f);
+        System.out.println("float value:"+c);
+        System.out.println("boolean result:"+d);
+        System.out.println("string value"+name);
+
+
+    }}
