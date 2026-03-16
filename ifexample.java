b3fdae9d93f33999b54a8f1fc41d151d210dc9ad
parameshs1821-del <parameshs1821@gmail.com>
2026-03-16 23:56:20 +0530

    Updated Java practice files

---
+++ ifexample.java
@@ -0,0 +1,16 @@
+public class ifexample {
+    public static void main(String[] args){
+        int age = 10;
+        if(age >=18){
+            System.out.println("You are eligibale to vote");
+        }else if(age <=10){
+            System.out.println("you are a child ");
+        }
+        else{
+            System.out.println("You are not able to vote");
+        }
+
+        }
+    }
+    
+
