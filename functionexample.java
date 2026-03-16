296585c123e369f89d7e6e12c97f96cca3274f72
parameshs1821-del <parameshs1821@gmail.com>
2026-03-17 00:11:09 +0530

    Updated Java practice files

---
+++ functionexample.java
@@ -0,0 +1,8 @@
+public class functionexample {
+    public static void  greet(){
+        System.out.println("Hello , welcome to my github account");
+    }
+public static void main(String[] args) {
+    greet();
+}}
+    
